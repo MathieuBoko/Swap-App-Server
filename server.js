28d599f41cdcb1951fe388ce4848054b44bc8825
Matheus <MathieuBk0@gmail.com>
2024-05-31 14:33:55 +0100

    Adjusting to new frontend features

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'test',
-    database: 'swap-app',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
   },
 });
 
